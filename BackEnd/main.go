e582880b0bf44e09cd0f56d83a6659a476ba2419
Timothy Stanley <79507886+Stanley77-web@users.noreply.github.com>
2022-04-26 19:40:24 +0700

    code

--- BackEnd/main.go
+++ BackEnd/main.go
@@ -5,7 +5,6 @@ import (
 	// "KobokDNA.com/Handlers"
 	"fmt"
 	"log"
-	"os"
 
 	"KobokDNA.com/Helper/GlobalVar"
 	"KobokDNA.com/Routes/ver1"
@@ -50,7 +49,7 @@ func main() {
 	// router.POST("/Searching", Handlers.PostSearching)
 
 	fmt.Println("Hallo")
-	GlobalVar.Server.Run(os.Getenv("PORT"))
+	GlobalVar.Server.Run()
 	// foundKMP, firstPosKMP, closedMatchKMP := Script.KnuthMorrisPratt("abacaabaccabacabaabb", "abacabb")
 	// if foundKMP {
 	// 	fmt.Printf("Found match at first index: %d\n", firstPosKMP)
