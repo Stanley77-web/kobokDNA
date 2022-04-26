20e1a5be155fae18de4a02cd9d3e2cbc4e935442
Timothy Stanley <79507886+Stanley77-web@users.noreply.github.com>
2022-04-26 16:30:31 +0700

    code

--- BackEnd/main.go
+++ BackEnd/main.go
@@ -49,7 +49,7 @@ func main() {
 	// router.POST("/Searching", Handlers.PostSearching)
 
 	fmt.Println("Hallo")
-	GlobalVar.Server.Run(":8080")
+	GlobalVar.Server.Run()
 	// foundKMP, firstPosKMP, closedMatchKMP := Script.KnuthMorrisPratt("abacaabaccabacabaabb", "abacabb")
 	// if foundKMP {
 	// 	fmt.Printf("Found match at first index: %d\n", firstPosKMP)
